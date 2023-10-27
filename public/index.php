205782b37a6f5fa41a2ba2fd87a6575cc5a72abb
Sara <34916@ma-web.nl>
2023-10-27 20:12:26 +0200

    20:12

--- public/index.php
+++ public/index.php
@@ -9,19 +9,32 @@
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Arimo:wght@700&display=swap" rel="stylesheet">
-    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="csss/style.css">
     <script src="js/main.js" defer></script>
     <title>NVVN SDG's</title>
 </head>
 <body>
-    <nav class="nav">NAVIGATIE</nav>
+    <header class="header">
+        <ul class="header__navigation">
+        </ul>
+    </header>
     <header class="header">CTA</header>
+    <section class="heading">
+        <div class="heading__div">
+            <h1 class="heading__divH1"></h1>
+            <button class="cta"></button>
+        </div>
+    </section>
     <main class="main">
-        <section class="sdg"></section>
+        <section class="sdg">
+            <article class="sdg__card"></article>
+            <article class="sdg__card"></article>
+            <article class="sdg__card"></article>
+            <div class="sdg__arrows"></div>
+        </section>
         <section class="over">
-            
         </section>
     </main>
-    <footer class="footer">CONTACTINFO</footer>
+    <footer class="footer"></footer>
 </body>
 </html>
