56b96d8cff8ca01843775950516f2071a60881c1
Sara <34916@ma-web.nl>
2023-11-03 17:59:03 +0100

    17:58

--- source/card.php
+++ source/card.php
@@ -17,8 +17,10 @@
         if ($result->num_rows > 0) {
             while ($row = $result->fetch_assoc()) {
                 if ($count < $limit) {
+                    $sdgImage = $row['photo'];
+                    $base64Image = base64_encode($sdgImage);
                     echo '<article class="sdg__card" data-sdg-id="'.$row['id'].'">';
-                    // echo '<img class="sdg__photo" src="'.$row['photo'].'">';
+                    echo '<img class="sdg__card--photo" src="data:image/webp;base64,'.$base64Image.'">';
                     echo '<h2 class="sdg__cardH2">'.$row['title'].'</h2>';
                     echo '<p class="sdg__card--intro">'.$row['introText'].'</p>';
                     echo '<p class="sdg__cardLink">Klik hier voor meer informatie</p>';
@@ -44,7 +46,7 @@
     //     foreach($SDGLibary as $sdg);
     //     echo '<article class="sdgOverzicht__card">';
     //     echo '<figure class="sdgOverzicht__cardFigure">';
-    //     echo '<img src='.$sdg['photo'].'alt="" class="sdgOverzcht__cardFigure--img">';
+    //     echo '<img src='.$sdg['photo'].'alt="" class="sdgOverzicht__cardFigure--img">';
     //     echo '</figure>';
     //     echo '<div class="sdgOverzicht__cardDiv">';
     //     echo '<h2 class="sdgOverzicht__cardDiv--title">'.$sdg['title'].'</h2>';
